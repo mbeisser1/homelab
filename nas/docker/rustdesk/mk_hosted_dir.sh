2841d959602b4a92173f956ae4764ec2303c7771
Matt Beisser <225018+mbeisser1@users.noreply.github.com>
2025-10-29 21:29:01 -0400

    Organize docker compose

--- nas/docker/rustdesk/mk_hosted_dir.sh
+++
@@ -1,8 +0,0 @@
-# Create the base directory for rustdesk
-sudo mkdir -p /pool/hosted/docker/rustdesk/data
-
-# Change ownership to root:hosted
-sudo chown -R root:hosted /pool/hosted/docker/rustdesk
-
-# Set permissions with SGID bit so group 'hosted' inherits
-sudo chmod -R 2775 /pool/hosted/docker/rustdesk
